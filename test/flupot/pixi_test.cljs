1d26f77eb947f925db909e08d0dbf97e452abf72
James Reeves <jreeves@weavejester.com>
2016-05-27 04:01:18 +0100

    Initial commit

---
+++ test/flupot/pixi_test.cljs
@@ -0,0 +1,7 @@
+(ns flupot.pixi-test
+  (:require [cljs.test :refer-macros [deftest is testing]]
+            [flupot.pixi :as pixi]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
