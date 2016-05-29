700b83dcaacde671eb55c38d5b35100e1291e147
James Reeves <jreeves@weavejester.com>
2016-05-29 04:15:24 +0100

    Wrap example in div to test React compatibility

--- example/src/example/core.cljs
+++ example/src/example/core.cljs
@@ -1,13 +1,16 @@
 (ns example.core
   (:require [brutha.core :as br]
-            [flupot.pixi :as pixi]))
+            [flupot.pixi :as pixi]
+            [flupot.dom :as dom]))
 
 (enable-console-print!)
 
 (defn content []
-  (pixi/stage
-   {:width 400, :height 300}
-   (pixi/text {:x 100, :y 100, :text "Hello World"})))
+  (dom/div
+   (dom/p "Example")
+   (pixi/stage
+    {:width 400, :height 300}
+    (pixi/text {:x 100, :y 100, :text "Hello World"}))))
 
 (let [app (.getElementById js/document "app")]
   (br/mount (content) app))
