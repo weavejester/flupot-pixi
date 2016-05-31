a1cab6a433f5c2427f161b29cfd8c69a62c5bf4e
James Reeves <jreeves@weavejester.com>
2016-05-31 02:10:24 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject flupot/pixi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A ClojureScript wrapper around the react-pixi library"
+  :url "https://github.com/weavejester/flupot-pixi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
